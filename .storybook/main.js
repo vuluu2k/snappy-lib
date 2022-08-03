01d40010c98f98132f60db61b2fc8a69199abc12
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2022-08-04 00:28:12 +0700

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,16 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-docs"
   ],
-  "framework": "@storybook/react",
+  framework: "@storybook/react",
+  features: {
+    previewMdx2: true,
+  },
 }
